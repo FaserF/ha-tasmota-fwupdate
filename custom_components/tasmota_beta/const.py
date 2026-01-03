eda74c57d43d42fe97097b2610e34c6aead9e2ce
Fabian Seitz <seitzf1@yahoo.de>
2026-01-03 19:10:24 +0100

    latest changes from new PR

---
+++ custom_components/tasmota_beta/const.py
@@ -0,0 +1,25 @@
+"""Constants used by multiple Tasmota modules."""
+
+from homeassistant.const import Platform
+
+CONF_DISCOVERY_PREFIX = "discovery_prefix"
+
+DATA_REMOVE_DISCOVER_COMPONENT = "tasmota_discover_{}"
+DATA_UNSUB = "tasmota_subscriptions"
+
+DEFAULT_PREFIX = "tasmota/discovery"
+
+DOMAIN = "tasmota_beta"
+
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.CAMERA,
+    Platform.COVER,
+    Platform.FAN,
+    Platform.LIGHT,
+    Platform.SENSOR,
+    Platform.SWITCH,
+    Platform.UPDATE,
+]
+
+TASMOTA_EVENT = "tasmota_event"
