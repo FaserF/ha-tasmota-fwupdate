e3ea88358c7744dafa7716547623cedee2c81bff
Fabian Seitz <fabian.seitz@pari.com>
2026-04-10 09:18:39 +0200

    remove duplicated folder

--- custom_components/tasmota_beta/hatasmota/hatasmota/__init__.py
+++
@@ -1,9 +0,0 @@
-"""HATasmota."""
-from .const import COMMAND_UPGRADE
-from .update import TasmotaUpdate, TasmotaUpdateConfig
-
-__all__ = [
-    "COMMAND_UPGRADE",
-    "TasmotaUpdate",
-    "TasmotaUpdateConfig",
-]
